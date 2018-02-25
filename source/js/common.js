9f6276f54a9badfe20f108cb1a290efe6ca21ccf
lubamark <lubamark@mail.ru>
2018-02-25 18:21:24 +0700

    оптимизирует js

--- source/js/common.js
+++ source/js/common.js
@@ -1,5 +1,4 @@
 //Moдальное окно
-
 var addButton =
 document.querySelectorAll('.js-modal-add-button');
 var overlay = document.querySelector('.modal');
@@ -14,7 +13,6 @@ if (addButton) {
     });
   };
 };
-
 if (overlay && addModal) {
   window.addEventListener('keydown', function(evt) {
     if (overlay.classList.contains('modal--show') && addModal.classList.contains('modal-add--show')) {
@@ -25,20 +23,20 @@ if (overlay && addModal) {
     }
   });
 }
-
-//выпадающее меню
+//Выпадающее меню
 var menuButton = document.querySelector('.main-nav__toggle');
 var menuWrapper = document.querySelectorAll('.main-nav__list');
 
-if (menuButton.classList.contains('main-nav__toggle--close') && menuWrapper[0].classList.contains('main-nav__list--open')  && menuWrapper[1].classList.contains('main-nav__list--open')) {
+if (menuButton.classList.contains('main-nav__toggle--close')) {
   menuButton.classList.remove('main-nav__toggle--close');
-  menuWrapper[0].classList.remove('main-nav__list--open');
-  menuWrapper[1].classList.remove('main-nav__list--open');
-};
-
+  for (i = 0; i < menuWrapper.length; i++) {
+    menuWrapper[i].classList.remove('main-nav__list--open');
+  }
+}
 menuButton.addEventListener('click', function(evt) {
   evt.preventDefault();
   menuButton.classList.toggle('main-nav__toggle--close');
-  menuWrapper[0].classList.toggle('main-nav__list--open');
-  menuWrapper[1].classList.toggle('main-nav__list--open');
-})
+  for (i = 0; i < menuWrapper.length; i++) {
+    menuWrapper[i].classList.toggle('main-nav__list--open');
+  };
+});
